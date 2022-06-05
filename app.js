2e7adf7c5623c1a50c594d5406d2a8752916c75d
Matheus-IT <matheuscosta.tech@gmail.com>
2022-06-05 11:48:12 -0300

    New routes for get and post

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/assets/"));
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+	const { firstName, lastName, email } = req.body;
+	res.send(`${firstName} ${lastName} ${email}`);
 });
 
 const port = 3000;
